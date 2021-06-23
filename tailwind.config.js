2d412553de33c68877896ae2caf2be934d157290
chiyoku <chiyokuoni@gmail.com>
2021-06-23 12:53:20 -0300

    :art: | Modified tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        pink: '0px 4px 14px rgba(255, 109, 214, 0.3);',
+      },
+      colors: {
+        primary: '#131518',
+        pink: '#FF6DD6',
+        gray: '#A4A4A4',
+      },
+      backgroundImage: (_) => ({
+        'black-gradient': 'linear-gradient(90deg, #15161C 0%, #0D1015 100%);',
+      }),
+    },
+    fontFamily: {
+      sans: ['Poppins'],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
